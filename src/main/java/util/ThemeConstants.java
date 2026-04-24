e9596e550eac6bdd5846adc2e46800f6d4f3c4a6
ReqiFauzan <reqifauzan233@gmail.com>
2026-04-25 00:46:19 +0700

    Create ThemeConstants class

---
+++ src/main/java/util/ThemeConstants.java
@@ -0,0 +1,7 @@
+public class ThemeConstants {
+    public static final String PRIMARY_COLOR = "#3498db";
+    public static final String SECONDARY_COLOR = "#2ecc71";
+    public static final String FONT_FAMILY = "Arial, sans-serif";
+    public static final int BUTTON_HEIGHT = 40;
+    public static final int BUTTON_WIDTH = 100;
+}
