cf51be8e2447bb68d86ceff63c6cbbba690e06e6
ReqiFauzan <reqifauzan233@gmail.com>
2026-04-25 00:45:38 +0700

    Create ThemeConstants.java with color constants and theme definitions

---
+++ src/util/ThemeConstants.java
@@ -0,0 +1,14 @@
+package util;
+
+public class ThemeConstants {
+    // Color constants
+    public static final String PRIMARY_COLOR = "#3498db";
+    public static final String SECONDARY_COLOR = "#2ecc71";
+    public static final String ACCENT_COLOR = "#e74c3c";
+
+    // Theme definitions
+    public static final String THEME_LIGHT = "light";
+    public static final String THEME_DARK = "dark";
+
+    // Add more constants as needed
+}
